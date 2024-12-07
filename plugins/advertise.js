38a2478009b428a84c7e33389e2fe8652f6d4393
Supersaiyan19 <anthony.campbel246@outlook.com>
2024-12-07 06:51:36 +0100

    Pʟᴀᴛɪɴᴜᴍ-V1

---
+++ plugins/advertise.js
@@ -0,0 +1,29 @@
+const { bot } = require('../lib/plugins.js');
+const { delay } = require('baileys');
+
+bot(
+	{
+		pattern: 'advertise',
+		isPublic: false,
+		desc: 'Create and Share Advertisement Messages to all Your Groups',
+		type: 'group',
+	},
+	async (message, match) => {
+		const adMsg = match || message.reply_message?.text;
+		if (!adMsg) return message.send('_I need text to advertise!_');
+		const groups = await message.client.groupFetchAllParticipating();
+		const groupDetails = Object.values(groups);
+		const groupIds = groupDetails.map(group => group.id);
+		await message.send(`_Broadcasting to ${groupIds.length} groups. Estimated completion in ${groupIds.length * 1.5} seconds_`);
+		const broadcastMessage = `\`\`\`*Broadcast*\n\n*Message:*\`\`\`` + adMsg;
+		const messageOptions = {
+			forwardingScore: 9999999,
+			isForwarded: true,
+		};
+		for (const groupId of groupIds) {
+			await delay(1500);
+			await message.client.sendMessage(groupId, { text: broadcastMessage, contextInfo: messageOptions });
+		}
+		return await message.send(`_Advertised Message to ${groupIds.length} Groups_`);
+	},
+);
